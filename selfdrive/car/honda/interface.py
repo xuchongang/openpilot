8f49f5ce60ee43ebda6f92b6efbfd071c5fe618e
Comma Device <device@comma.ai>
2020-02-25 22:22:06 -0700

    restore kf to the default value

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -286,7 +286,7 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.41
       ret.steerRatio = 17.45  # 14.35 as spec
       tire_stiffness_factor = 0.82
-      ret.lateralTuning.pid.kf = 0.00004  # conservative feed-forward
+      ret.lateralTuning.pid.kf = 0.00006  # conservative feed-forward
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.45], [0.135]]
       ret.longitudinalTuning.kpBP = [0., 5., 35.]
       ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
